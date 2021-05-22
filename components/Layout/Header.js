37a7f58fbdf730e990e69becdce1d50425ed57ae
Denis Onsare <denisonsare@gmail.com>
2021-05-22 19:51:23 +0300

    Header: Link navbar to respective pages

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,17 +1,30 @@
 import React from "react";
+import Link from "next/link";
 
 function Header() {
   return (
     <div className="header">
-      <div className="btn">
-        <img src="./icons/user.png" alt="profile" />
-      </div>
-      <div className="logo">
-        <img src="./icons/logo.png" alt="Logo" />
-      </div>
-      <div className="btn">
-        <img src="./icons/messages.png" alt="messages" />
-      </div>
+      <Link href="/profile">
+        <a>
+          <div className="btn">
+            <img src="./icons/user.png" alt="profile" />
+          </div>
+        </a>
+      </Link>
+      <Link href="/">
+        <a>
+          <div className="logo">
+            <img src="./icons/logo.png" alt="Logo" />
+          </div>
+        </a>
+      </Link>
+      <Link href="/chat">
+        <a>
+          <div className="btn">
+            <img src="./icons/messages.png" alt="messages" />
+          </div>
+        </a>
+      </Link>
     </div>
   );
 }
