f43f52d844d05d83196daa1afde6770f95f24c32
Denis Onsare <denisonsare@gmail.com>
2021-05-22 13:39:31 +0300

    Initial commit

---
+++ components/Layout/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Head from "next/head";
+
+import Header from "./Header";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <title>Cat Love</title>
+        <meta name="description" content="Tinder for cats" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
