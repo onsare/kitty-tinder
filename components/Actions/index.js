c2bf77972692a9d4384b6e9ed38530252ea0ceff
Denis Onsare <denisonsare@gmail.com>
2021-05-22 15:15:19 +0300

    Style user action buttons

--- components/Actions/index.js
+++ components/Actions/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Actions() {
-  return <div>user actions</div>;
+  return (
+    <div className="actions">
+      <button>
+        <img src="/icons/rewind.png" alt="rewind user" />
+      </button>
+      <button>
+        <img src="/icons/dislike.png" alt="dislike user" />
+      </button>
+      <button>
+        <img src="/icons/like.png" alt="like user" />
+      </button>
+      <button>
+        <img src="/icons/superlike.png" alt="superlike user" />
+      </button>
+    </div>
+  );
 }
 
 export default Actions;
