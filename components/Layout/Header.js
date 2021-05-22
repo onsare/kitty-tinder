4f6bc3b0da475d5eb26d5ac1847565fc64f6f35d
Denis Onsare <denisonsare@gmail.com>
2021-05-22 13:56:04 +0300

    Added application icons

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Header() {
-  return <div className="header">header</div>;
+  return (
+    <div className="header">
+      <div>
+        <button>Profile</button>
+      </div>
+      <div>
+        <button>logo</button>
+      </div>
+      <div>messages</div>
+    </div>
+  );
 }
 
 export default Header;
