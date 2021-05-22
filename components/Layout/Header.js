5431eb8e5cb17c0b7836be0840dde3436c84befc
Denis Onsare <denisonsare@gmail.com>
2021-05-22 14:09:52 +0300

    Modified: header styles; adds icons

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,13 +3,15 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      <div>
-        <button>Profile</button>
+      <div className="btn">
+        <img src="./icons/user.png" alt="profile" />
       </div>
-      <div>
-        <button>logo</button>
+      <div className="logo">
+        <img src="./icons/logo.png" alt="Logo" />
       </div>
-      <div>messages</div>
+      <div className="btn">
+        <img src="./icons/messages.png" alt="messages" />
+      </div>
     </div>
   );
 }
