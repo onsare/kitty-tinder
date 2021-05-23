5373d7dda03879dd4b1fbfaa2673644cfda29db3
Denis Onsare <denisonsare@gmail.com>
2021-05-24 00:06:15 +0300

    onswipe save liked or disliked users to state

--- components/Decks/index.js
+++ components/Decks/index.js
@@ -1,14 +1,30 @@
 import React, { useState, useMemo } from "react";
+import { useContext } from "react";
+
 import Card from "./Card";
 import TinderCard from "react-tinder-card";
 
+import { AppContext } from "../../context/AppContext";
+
 function Decks({ data }) {
+  const likedUsers = [];
+  const dislikedUsers = [];
+  const { setLiked, setDisliked } = useContext(AppContext);
+
   const onCardLeftScreen = (myIdentifier) => {
     console.log(myIdentifier + " left the screen");
   };
-  const onSwipe = (direction) => {
-    console.log("You swiped: " + direction);
+
+  const swiped = (direction, cat) => {
+    if (direction === "right") {
+      likedUsers.push(cat);
+      setLiked([...likedUsers, cat]);
+    } else {
+      dislikedUsers.push(cat);
+      setDisliked([...dislikedUsers, cat]);
+    }
   };
+
   return (
     <div className="decks">
       {data.map((cat) => (
@@ -16,7 +32,7 @@ function Decks({ data }) {
           key={cat.id}
           className="swipe "
           preventSwipe={["up", "down"]}
-          onSwipe={onSwipe}
+          onSwipe={(dir) => swiped(dir, cat)}
           onCardLeftScreen={() => onCardLeftScreen(cat.id)}
         >
           <Card picture={cat.url} />
